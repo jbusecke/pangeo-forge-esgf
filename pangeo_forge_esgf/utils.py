814d6914d3b6cc28f9a147faaee6cc3f0074fab5
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2022-10-03 13:51:02 +0200

    API update for test_script

--- pangeo_forge_esgf/utils.py
+++ pangeo_forge_esgf/utils.py
@@ -2,8 +2,11 @@ from typing import Dict
 from .params import id_templates
 
 
-def facets_from_iid(iid: str, mip: str) -> Dict[str, str]:
+def facets_from_iid(iid: str, mip: str = None) -> Dict[str, str]:
     """Translates iid string to facet dict according to CMIP6 naming scheme"""
+    if mip is None:
+        # take project id from first iid entry by default
+        mip = iid.split(".")[0]
     iid_name_template = id_templates[mip]
     facets = {}
     for name, value in zip(iid_name_template.split("."), iid.split(".")):
