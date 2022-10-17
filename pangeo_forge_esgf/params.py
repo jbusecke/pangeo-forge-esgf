50def3bd1ca72fe752a85cbcd39e3315737600d4
github action <5659125+larsbuntemeyer@users.noreply.github.com>
2022-10-17 23:44:09 +0200

    added base_dict to params

--- pangeo_forge_esgf/params.py
+++ pangeo_forge_esgf/params.py
@@ -3,17 +3,7 @@ cmip6_template = "mip_era.activity_id.institution_id.source_id.experiment_id.var
 cordex_template = "project.product.domain.institute.driving_model.experiment.ensemble.rcm_name.rcm_version.time_frequency.variable"
 
 # request params
-cmip6_params = {
-    "type": "Dataset",
-    "retracted": "false",
-    "format": "application/solr+json",
-    "fields": "instance_id",
-    "latest": "true",
-    "distrib": "true",
-    "limit": 500,
-}
-
-cordex_params = {
+base_params = {
     "type": "Dataset",
     "format": "application/solr+json",
     "fields": "instance_id",
@@ -22,6 +12,10 @@ cordex_params = {
     "limit": 500,
 }
 
+cmip6_params = base_params | {"retracted": "false"}
+cordex_params = base_params.copy()
+
+# this needs refactoring when you request the dataset_id template from ESGF servers
 id_templates = {"CMIP6": cmip6_template, "CORDEX": cordex_template}
 
 request_params = {"CMIP6": cmip6_params, "CORDEX": cordex_params}
