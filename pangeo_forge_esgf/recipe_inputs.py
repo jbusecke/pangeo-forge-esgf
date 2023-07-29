172f1d0d3ec6582c31081ab3ab86eb8e404c9022
jbusecke <julius@ldeo.columbia.edu>
2023-07-29 12:46:09 +0200

    handoff for a round of recipe testing

--- pangeo_forge_esgf/recipe_inputs.py
+++ pangeo_forge_esgf/recipe_inputs.py
@@ -45,8 +45,7 @@ async def generate_urls_from_iids(
     """
     # Lets limit the amount of connections to avoid being flagged
     connector = aiohttp.TCPConnector(
-        # limit_per_host=10
-        limit_per_host=100
+        limit_per_host=50
     )  # Not sure we need a timeout now, but this might be useful in the future
     # combined with a retry.
     # timeout = aiohttp.ClientTimeout(total=40)
@@ -86,6 +85,7 @@ async def iid_request(
     else: 
         print(f"{iid=}: Getting urls")
         urls = [r["url"] for r in filtered_response_data]
+        print(f"{iid=}: DEBUG: WHAT ELSE is in a response? {filtered_response_data[0]}")
         print(f"{iid=}: Found {len(urls)} urls {urls=}")
 
     return urls
@@ -137,12 +137,12 @@ def backoff_hdlr(details):
 @backoff.on_exception(
         backoff.expo,
         aiohttp.ClientError,
-        max_tries=3,
-        max_time=300, 
+        max_tries=5,
+        max_time=60, 
         on_backoff=backoff_hdlr
         )
 async def check_url(iid: str, url: str, session: aiohttp.ClientSession, timeout: int) -> int:
-    async with session.head(url, timeout=10, raise_for_status=True) as resp:
+    async with session.head(url, timeout=timeout, raise_for_status=True) as resp:
         return resp.status
 
 async def sort_and_filter_response(
@@ -201,6 +201,36 @@ def _pick_url_type(response):
     else:
         raise ValueError("This recipe currently only supports HTTP links")
 
+async def choose_data_nodes_for_file(
+            iid: str,
+            filename: str,
+            response_list: List[Dict[str, str]],
+            session: aiohttp.ClientSession,
+    ) -> Dict[str, str]:
+    """For a given filename and list of responses 
+        (representing the same file on different data urls) and 
+        return the first response with a responsive url"""
+    print(f"{iid=} available data nodes for {filename}: {[r['data_node'] for r in response_list]}")
+    valid_responses = []
+    for r in response_list:
+        data_node = r['data_node']
+        # TODO: If this is successful I could implement a 'preferred data node' logic here as well. 
+        # First check the urls matching the preferred data node, then check the rest.
+        url = r['url']
+        try:
+            status = await check_url(iid, url, session, timeout=60) # maybe i coudl implement a backoff with increasing timeout here?
+            # TODO: It would be neat if we can do this checking concurrently and cancel once we get a response.
+            # This would maybe self-select the preferred data node in a way.
+            if status in [200, 206]:
+                valid_responses.append(r)
+                break
+        except Exception as e:
+            print(f"{iid=}: {data_node=} {url=} failed with {e}")
+    
+    if len(valid_responses) > 1:
+        raise ValueError(f"{iid=}: More than one valid response ({valid_responses}) found for {filename=}. This should not happen.")
+
+    return filename, valid_responses
 
 async def pick_data_node(
     response_groups: Dict[str, List[Dict[str, str]]],
@@ -228,123 +258,40 @@ async def pick_data_node(
     # TODO: We could make this logic easier by just taking the first available data node *per* file. 
     # This might also lead to unforseen issues, so lets for now say we want all files to be 
     # available on a single data node
-    def find_data_nodes(response_list:List[Dict[str, str]]) -> List[str]:
-        nodes = []
-        for r in response_list:
-                    dn = r['data_node']
-                    if dn not in preferred_data_nodes:
-                        nodes.append(dn)
-        return nodes
+         
+
+    print(f"{iid=}: Allowing mixed data nodes")
+    # Try the data nodes for each filename concurrently
+    tasks = []
+    for filename, response_list in response_groups.items(): #TODO: This could be done concurrently for each filename to save time
+        print(f"{iid=} Start evaluating valid data_nodes for {filename}")
+        tasks.append(
+            asyncio.ensure_future(
+            choose_data_nodes_for_file(
+            iid,
+            filename,
+            response_list,
+            session
+            )))
+    valid_responses = await asyncio.gather(*tasks)
     
-    # tasks = []
-    # for iid in iid_list:
-    #     tasks.append(asyncio.ensure_future(iid_request(session, iid, search_node)))
-
-    # raw_urls = await asyncio.gather(*tasks)
-
-    # async def get_data_nodes_for_file(
-    #         iid: str,
-    #         filename: str,
-    #         response_list: List[Dict[str, str]]
-    # ) -> Dict[str, str]:
-    #     """Check every url for a given filename and list of responses 
-    #      (representing the same file on different data urls) and 
-    #      return the first response with a responsive url """
-    #     valid_responses = []
-    #     for r in response_list: 
-    #         # TODO: If this is successful I could implement a 'preferred data node' logic here as well. 
-    #         # First check the urls matching the preferred data node, then check the rest.
-    #         url = r['url']
-    #         try:
-    #             status = await check_url(iid, url, session, timeout=10) # maybe i coudl implement a backoff with increasing timeout here?
-    #             # TODO: It would be neat if we can do this checking concurrently and cancel once we get a response.
-    #             # This would maybe self-select the preferred data node in a way.
-    #             if status in [200, 206]:
-    #                 valid_responses.append(r)
-    #                 break
-    #         except Exception as e:
-    #             print(f"{iid=}: Checking {url=} failed with {e}")
-            
-    #     #make sure that only one valid response was populated
-    #     if len(valid_responses) == 1:
-    #         return valid_responses[0]
-    #     elif len(valid_responses) >2:
-    #         raise ValueError(f"{iid=}: More than one valid response ({valid_responses}) found for {filename=}. This should not happen.")
-
-        
-
-    if allow_mixed_nodes:
-        print(f"{iid=}: Allowing mixed data nodes")
-        filename_response_groups = {}
-        tasks = []
-        for filename, response_list in response_groups.items(): #TODO: This could be done concurrently for each filename to save time
-            print('start filename blah blah')
-            
-            valid_responses = []
-            for r in response_list: 
-                # TODO: If this is successful I could implement a 'preferred data node' logic here as well. 
-                # First check the urls matching the preferred data node, then check the rest.
-                url = r['url']
-                try:
-                    status = await check_url(iid, url, session, timeout=10) # maybe i coudl implement a backoff with increasing timeout here?
-                    # TODO: It would be neat if we can do this checking concurrently and cancel once we get a response.
-                    # This would maybe self-select the preferred data node in a way.
-                    if status in [200, 206]:
-                        valid_responses.append(r)
-                        break
-                except Exception as e:
-                    print(f"{iid=}: Checking {url=} failed with {e}")
-                
-            #make sure that only one valid response was populated
-            if len(valid_responses) == 1:
-                filename_response_groups[filename] = valid_responses[0]
-            elif len(valid_responses) >2:
-                raise ValueError(f"{iid=}: More than one valid response ({valid_responses}) found for {filename=}. This should not happen.")
-
-        single_response_dict = filename_response_groups 
+    # parse gathered output into Dict[filename, single response]
+    single_response_dict = {
+        filename: responses[0] for filename, responses in valid_responses if len(responses)==1
+        }
+    
+    # Check that all files are available on the particular data_node
+    all_filenames = set(response_groups.keys())
+    found_filenames = set(single_response_dict.keys())
+    missing_filenames = list(
+        all_filenames - found_filenames
+        )
+    if len(missing_filenames) > 0:
+        print(f"{iid=}: Could only find {len(found_filenames)}/{len(all_filenames)} filenames. {missing_filenames=}")
+        return {}
+    else: 
+        print(f"{iid=}: Found a valid url for all files")
         # diagnose how many datanodes we used
         data_nodes_used = list(set([r['data_node'] for r in single_response_dict.values()]))
         print(f"{iid=}: Data Nodes used: {data_nodes_used}")
-    else:
-        # get all data nodes available
-        data_nodes = []
-        for filename, response_list in response_groups.items():
-            data_nodes += find_data_nodes(response_list)
-        data_nodes = list(set(data_nodes)) # only retain unique values
-        print(f"{iid=}: Data Nodes inferred from responses: {data_nodes}")
-        # now concat the preferred and inferred nodes, making sure that the preferred are looped over first
-        data_nodes = preferred_data_nodes + data_nodes
-
-        # split response groups by data node
-        data_node_response_group = {}
-        for data_node in data_nodes:
-            print(f"{iid=}: Checking {data_node=}")
-            data_node_dict = {}
-            for filename, response_list in response_groups.items():
-                matching_responses = []
-                for r in response_list:
-                    if r['data_node'] == data_node:
-                        status = await check_url(r["url"], session)
-                        if status in [200, 308]:
-                            matching_responses.append(r)
-                if len(matching_responses) == 1:
-                    data_node_dict[filename] = matching_responses[0]
-                elif len(matching_responses)>1:
-                    raise ValueError(
-                        f"{iid}: Found two urls for {filename=} and {data_node=}. Got {matching_responses}"
-                        )
-            # Check that all files are available on the particular data_node
-            all_filenames = set(response_groups.keys())
-            found_filenames = set(data_node_dict.keys())
-            missing_filenames = list(
-                all_filenames - found_filenames
-                )
-            if len(missing_filenames) > 0:
-                print(f"{iid=}: Could only find {len(found_filenames)}/{len(all_filenames)} filenames on {data_node=}. {missing_filenames=}")
-            else: 
-                data_node_response_group = data_node_dict
-                print(f"{iid=}: Found all files for {data_node=}")
-                break
-        single_response_dict = data_node_response_group
-
-    return single_response_dict
+        return single_response_dict
