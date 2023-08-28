5cc7c2c53e7e24eeb054a031cff717dd1dbc0d5d
jbusecke <julius@ldeo.columbia.edu>
2023-08-28 16:27:19 -0400

    Factor out naming scheme

--- pangeo_forge_esgf/utils.py
+++ pangeo_forge_esgf/utils.py
@@ -1,9 +1,11 @@
 from typing import Dict
 
+CMIP6_naming_scheme = "mip_era.activity_id.institution_id.source_id.experiment_id.variant_label.table_id.variable_id.grid_label.version"
+
 
 def facets_from_iid(iid: str) -> Dict[str, str]:
     """Translates iid string to facet dict according to CMIP6 naming scheme"""
-    iid_name_template = "mip_era.activity_id.institution_id.source_id.experiment_id.variant_label.table_id.variable_id.grid_label.version"
+    iid_name_template = CMIP6_naming_scheme
     facets = {}
     for name, value in zip(iid_name_template.split("."), iid.split(".")):
         facets[name] = value
