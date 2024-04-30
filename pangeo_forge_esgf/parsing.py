dc49b28095ec1c7d7aff369d91530085f9581c9c
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-30 10:55:23 -0400

    Multi-node parsing

--- pangeo_forge_esgf/parsing.py
+++ pangeo_forge_esgf/parsing.py
@@ -47,27 +47,35 @@ def split_square_brackets(facet_string: str) -> List[str]:
     return split_iid_combinations
 
 
-def parse_instance_ids(iid_string: str, search_node: Optional[str] = None) -> list[str]:
+def parse_instance_ids(iid_string: str, search_nodes: Optional[list[str]] = None) -> list[str]:
     """Parse an instance id with wildcards"""
-    if search_node is None:
-        # search_node = "https://esgf-node.llnl.gov/esg-search/search"
-        search_node = "https://esgf-data.dkrz.de/esg-search/search"
-        # FIXME: I got some really weird flakyness with the LLNL node. This is a dumb way to test this...
-
+    # I am never sure where to get the full list of SOLR indicies, took this from intake-esgf: https://intake-esgf.readthedocs.io/en/latest/configure.html
+    if search_nodes is None:
+        search_nodes = [
+            "https://esgf-node.llnl.gov/esg-search/search",
+            "https://esgf-data.dkrz.de/esg-search/search",
+            "https://esgf.nci.org.au/esg-search/search",
+            "https://esgf-node.ornl.gov/esg-search/search",
+            "https://esgf-node.ipsl.upmc.fr/esg-search/search",
+            "https://esg-dn1.nsc.liu.se/esg-search/search",
+            "https://esgf.ceda.ac.uk/esg-search/search",
+        ]
+        
     # first resolve the square brackets
     split_iids: List[str] = split_square_brackets(iid_string)
 
     parsed_iids: List[str] = []
     for iid in split_iids:
-        facets = facets_from_iid(iid)
-        facets_filtered = {
-            k: v for k, v in facets.items() if v != "*"
-        }  # leaving out the wildcards here will just request everything for that facet
-
-        resp = request_from_facets(search_node, **facets_filtered)
-        if resp.status_code != 200:
-            print(f"Request [{resp.url}] failed with {resp.status_code}")
-        else:
-            json_dict = resp.json()
-            parsed_iids.extend(instance_ids_from_request(json_dict))
-    return parsed_iids
+        for node in search_nodes:
+            facets = facets_from_iid(iid)
+            facets_filtered = {
+                k: v for k, v in facets.items() if v != "*"
+            }  # leaving out the wildcards here will just request everything for that facet
+    
+            resp = request_from_facets(search_node, **facets_filtered)
+            if resp.status_code != 200:
+                print(f"Request [{resp.url}] failed with {resp.status_code}")
+            else:
+                json_dict = resp.json()
+                parsed_iids.extend(instance_ids_from_request(json_dict))
+    return list(set(parsed_iids))
