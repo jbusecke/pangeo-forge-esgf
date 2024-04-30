858a2d57952ae1936a68d1e66c2d8ae33fdb3463
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-30 11:05:39 -0400

    Deprecation warning and backwards compatibility

--- pangeo_forge_esgf/parsing.py
+++ pangeo_forge_esgf/parsing.py
@@ -1,4 +1,5 @@
 import requests
+import warnings
 from typing import Optional, List
 
 from .utils import facets_from_iid
@@ -48,9 +49,21 @@ def split_square_brackets(facet_string: str) -> List[str]:
 
 
 def parse_instance_ids(
-    iid_string: str, search_nodes: Optional[list[str]] = None
+    iid_string: str,
+    search_nodes: Optional[list[str]] = None,
+    search_node: Optional[str] = None,
 ) -> list[str]:
     """Parse an instance id with wildcards"""
+    if search_node is not None:
+        warnings.warn(
+            "`search_node` is being deprecated. Please provide a list of urls to `search_nodes` instead", 
+            DeprecationWarning
+        )
+        # make this backwards compatible
+        if search_nodes is None:
+            search_nodes = [search_node]
+
+    
     # I am never sure where to get the full list of SOLR indicies, took this from intake-esgf: https://intake-esgf.readthedocs.io/en/latest/configure.html
     if search_nodes is None:
         search_nodes = [
